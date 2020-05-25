594e5f64eea3d89a2f630637b4901cffba07040f
Santiago Marrone <santiago.marrone@ey.com>
2020-05-25 12:28:24 -0300

    Add constructor method for deck type

--- cards/deck.go
+++ cards/deck.go
@@ -4,6 +4,23 @@ import "fmt"
 
 type deck []string 
 
+func newDeck() deck {
+
+	d := deck{}
+
+	cardSuits := []string{"Clubs", "Diamonds", "Hearts", "Spades"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Joker", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+
+			d = append(d, value+" of "+suit)
+		}
+	}
+
+	return d
+}
+
 func (d deck) print() {
 
 	for i, card := range d {
