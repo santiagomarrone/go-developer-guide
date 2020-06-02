97d4895a1fd56271532eff9a1775a8e1c681cc79
Santiago Marrone <santiago.marrone@ey.com>
2020-06-02 20:55:25 -0300

    Save data to hard drive

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import(
+
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 type deck []string 
 
@@ -32,4 +37,14 @@ func (d deck) print() {
 func deal(d deck, handSize int) (deck, deck) {
 
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(file string) error {
+
+	return ioutil.WriteFile(file, []byte(d.toString()), 0666)
 }
--- cards/main.go
+++ cards/main.go
@@ -3,6 +3,7 @@ package main
 func main() {
 
 	cards := newDeck()
+	cards.saveToFile("cardsFile")
 
 	hand, remainingCards := deal(cards, 5)
 
